3977c05c3cfd7f6ef45ab4b94fefca2563d9f0a1
Ben Wilhelm <ben@doublebeamdesign.com>
2012-10-28 18:23:51 -0500

    scanner initial test

--- js/js.js
+++ js/js.js
@@ -10,7 +10,25 @@
     $('#status').appendLine('app ready') ;
     $('#scan_me').click(function(e){
       e.preventDefault() ;
-      $('#status').appendLine('click!') ;
+
+      console.log('scanning');
+      try {
+        window.plugins.barcodeScanner.scan(function(args) {
+          console.log("Scanner result: \n" +
+              "text: " + args.text + "\n" +
+              "format: " + args.format + "\n" +
+              "cancelled: " + args.cancelled + "\n");
+          /*
+          if (args.format == "QR_CODE") {
+              window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+          }
+          */
+          $('#status').html(args.text);
+          console.log(args);
+        });
+      } catch (ex) {
+        $('#status').appendLine(ex.message);
+      }
     })
   }) ;
   
